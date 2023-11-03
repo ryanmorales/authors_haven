3518ab2e31a679549a0bd72d6bc5e023f98dc632
Ryan Darren Morales <ryan.morales14@gmail.com>
2023-11-03 23:34:51 +0800

    Task: Add custom user model, redoc setup api docs

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -13,11 +13,37 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Authors Haven API",
+        default_version="v1",
+        description="API Endpoints for Authors Haven API Course",
+        contact=openapi.Contact(email="ryan.morales14@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     #path("admin/", admin.site.urls),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
+
+admin.site.site_header = "Authors Haven API Admin"
+admin.site.site_title = "Authors Haven API Admin Portal"
+admin.site.index_title = "Welcome to Authors Haven API Portal"
+
+
+
+
+
+
