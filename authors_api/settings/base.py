5c58983fe0c6445aa6f749451eaa468312de326b
Ryan Darren Morales <ryan.morales14@gmail.com>
2023-11-14 21:11:21 +0800

    Task: User adds, update, delete responses

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -62,6 +62,7 @@ LOCAL_APPS = [
     "core_apps.articles",
     "core_apps.ratings",
     "core_apps.bookmarks",
+    "core_apps.responses",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
